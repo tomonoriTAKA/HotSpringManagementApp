f04fd2ef0665d4e0278c99da4daa1992a30136c0
Tomonori <tomonori0204.takahashi@gmail.com>
2017-11-19 16:21:12 +0900

    Initial Commit

---
+++ HachimantaiHotSpringAppSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HachimantaiHotSpringAppSample
+//
+//  Created by 高橋知憲 on 2017/11/19.
+//  Copyright © 2017年 高橋知憲. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
